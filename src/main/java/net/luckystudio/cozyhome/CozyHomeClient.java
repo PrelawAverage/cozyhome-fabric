b782c011102e0f717d7516b517d2200a90ed6960
PrelawAverage <bryanguzman859@gmail.com>
2024-09-07 14:16:34 -0400

    Initial Commit

---
+++ src/main/java/net/luckystudio/cozyhome/CozyHomeClient.java
@@ -0,0 +1,10 @@
+package net.luckystudio.cozyhome;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CozyHomeClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/luckystudio/cozyhome/CozyHomeDataGenerator.java
@@ -0,0 +1,11 @@
+package net.luckystudio.cozyhome;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CozyHomeDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
