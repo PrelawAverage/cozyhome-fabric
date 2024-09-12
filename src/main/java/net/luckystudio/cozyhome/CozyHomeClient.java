d13fd3a185b386c6a3a9a07770f77e76f7606cd3
PrelawAverage <bryanguzman859@gmail.com>
2024-09-12 04:10:37 -0400

    - Updated the models and textures of the lamps to look much more appealing. - Changed all lamps to render as Translucent. - Added recipes for lamps - Added Loottables for lamps

--- src/main/java/net/luckystudio/cozyhome/CozyHomeClient.java
+++ src/main/java/net/luckystudio/cozyhome/CozyHomeClient.java
@@ -8,21 +8,20 @@ import net.minecraft.client.render.RenderLayer;
 public class CozyHomeClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.WHITE_LAMP, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.ORANGE_LAMP, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.MAGENTA_LAMP, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.LIGHT_BLUE_LAMP, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.YELLOW_LAMP, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.LIME_LAMP, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.PINK_LAMP, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.GRAY_LAMP, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.LIGHT_GRAY_LAMP, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.CYAN_LAMP, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.PURPLE_LAMP, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.BLUE_LAMP, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.BROWN_LAMP, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.GREEN_LAMP, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.RED_LAMP, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.BLACK_LAMP, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.WHITE_LAMP, RenderLayer.getTranslucent());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.ORANGE_LAMP, RenderLayer.getTranslucent());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.MAGENTA_LAMP, RenderLayer.getTranslucent());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.LIGHT_BLUE_LAMP, RenderLayer.getTranslucent());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.YELLOW_LAMP, RenderLayer.getTranslucent());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.LIME_LAMP, RenderLayer.getTranslucent());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.PINK_LAMP, RenderLayer.getTranslucent());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.GRAY_LAMP, RenderLayer.getTranslucent());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.LIGHT_GRAY_LAMP, RenderLayer.getTranslucent());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.CYAN_LAMP, RenderLayer.getTranslucent());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.PURPLE_LAMP, RenderLayer.getTranslucent());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.BLUE_LAMP, RenderLayer.getTranslucent());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.BROWN_LAMP, RenderLayer.getTranslucent());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.RED_LAMP, RenderLayer.getTranslucent());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.BLACK_LAMP, RenderLayer.getTranslucent());
     }
 }
--- src/main/java/net/luckystudio/cozyhome/datagen/ModRecipeProvider.java
+++ src/main/java/net/luckystudio/cozyhome/datagen/ModRecipeProvider.java
@@ -20,7 +20,7 @@ public class ModRecipeProvider extends FabricRecipeProvider {
         super(output, registriesFuture);
     }
 
-    public static void getPlankedWallRecipe(RecipeExporter exporter, RecipeCategory category, ItemConvertible output, ItemConvertible input) {
+    public static void offerPlankedWallRecipe(RecipeExporter exporter, RecipeCategory category, ItemConvertible output, ItemConvertible input) {
         ShapedRecipeJsonBuilder.create(
                 category,
                 output, 3)
@@ -31,19 +31,30 @@ public class ModRecipeProvider extends FabricRecipeProvider {
                 .criterion(hasItem(input), conditionsFromItem(input))
                 .offerTo(exporter);
     }
+    public static void offerLampRecipe(RecipeExporter exporter, RecipeCategory category, ItemConvertible output, ItemConvertible input1, ItemConvertible input2) {
+        ShapedRecipeJsonBuilder.create(
+                        category,
+                        output, 1)
+                .pattern("@")
+                .pattern("#")
+                .input('@', input1)
+                .input('#', input2)
+                .criterion(hasItem(Items.LANTERN), conditionsFromItem(Items.LANTERN))
+                .offerTo(exporter);
+    }
 
     @Override
     public void generate(RecipeExporter exporter) {
-        getPlankedWallRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, ModBlocks.OAK_PLANKED_WALL, Blocks.OAK_PLANKS);
-        getPlankedWallRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, ModBlocks.SPRUCE_PLANKED_WALL, Blocks.SPRUCE_PLANKS);
-        getPlankedWallRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, ModBlocks.BIRCH_PLANKED_WALL, Blocks.BIRCH_PLANKS);
-        getPlankedWallRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, ModBlocks.JUNGLE_PLANKED_WALL, Blocks.JUNGLE_PLANKS);
-        getPlankedWallRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, ModBlocks.ACACIA_PLANKED_WALL, Blocks.ACACIA_PLANKS);
-        getPlankedWallRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, ModBlocks.DARK_OAK_PLANKED_WALL, Blocks.DARK_OAK_PLANKS);
-        getPlankedWallRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, ModBlocks.MANGROVE_PLANKED_WALL, Blocks.MANGROVE_PLANKS);
-        getPlankedWallRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, ModBlocks.BAMBOO_PLANKED_WALL, Blocks.BAMBOO_PLANKS);
-        getPlankedWallRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, ModBlocks.CRIMSON_PLANKED_WALL, Blocks.CRIMSON_PLANKS);
-        getPlankedWallRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, ModBlocks.WARPED_PLANKED_WALL, Blocks.WARPED_PLANKS);
+        offerPlankedWallRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, ModBlocks.OAK_PLANKED_WALL, Blocks.OAK_PLANKS);
+        offerPlankedWallRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, ModBlocks.SPRUCE_PLANKED_WALL, Blocks.SPRUCE_PLANKS);
+        offerPlankedWallRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, ModBlocks.BIRCH_PLANKED_WALL, Blocks.BIRCH_PLANKS);
+        offerPlankedWallRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, ModBlocks.JUNGLE_PLANKED_WALL, Blocks.JUNGLE_PLANKS);
+        offerPlankedWallRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, ModBlocks.ACACIA_PLANKED_WALL, Blocks.ACACIA_PLANKS);
+        offerPlankedWallRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, ModBlocks.DARK_OAK_PLANKED_WALL, Blocks.DARK_OAK_PLANKS);
+        offerPlankedWallRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, ModBlocks.MANGROVE_PLANKED_WALL, Blocks.MANGROVE_PLANKS);
+        offerPlankedWallRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, ModBlocks.BAMBOO_PLANKED_WALL, Blocks.BAMBOO_PLANKS);
+        offerPlankedWallRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, ModBlocks.CRIMSON_PLANKED_WALL, Blocks.CRIMSON_PLANKS);
+        offerPlankedWallRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, ModBlocks.WARPED_PLANKED_WALL, Blocks.WARPED_PLANKS);
 
         offerSingleOutputShapelessRecipe(exporter, Items.OAK_PLANKS, ModBlocks.OAK_PLANKED_WALL, RecipeCategory.BUILDING_BLOCKS.getName());
         offerSingleOutputShapelessRecipe(exporter, Items.SPRUCE_PLANKS, ModBlocks.SPRUCE_PLANKED_WALL, RecipeCategory.BUILDING_BLOCKS.getName());
@@ -56,5 +67,22 @@ public class ModRecipeProvider extends FabricRecipeProvider {
         offerSingleOutputShapelessRecipe(exporter, Items.BAMBOO_PLANKS, ModBlocks.BAMBOO_PLANKED_WALL, RecipeCategory.BUILDING_BLOCKS.getName());
         offerSingleOutputShapelessRecipe(exporter, Items.CRIMSON_PLANKS, ModBlocks.CRIMSON_PLANKED_WALL, RecipeCategory.BUILDING_BLOCKS.getName());
         offerSingleOutputShapelessRecipe(exporter, Items.WARPED_PLANKS, ModBlocks.WARPED_PLANKED_WALL, RecipeCategory.BUILDING_BLOCKS.getName());
+
+        offerLampRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, ModBlocks.WHITE_LAMP, Items.RED_WOOL, Items.LANTERN);
+        offerLampRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, ModBlocks.ORANGE_LAMP, Items.ORANGE_WOOL, Items.LANTERN);
+        offerLampRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, ModBlocks.MAGENTA_LAMP, Items.MAGENTA_WOOL, Items.LANTERN);
+        offerLampRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, ModBlocks.LIGHT_BLUE_LAMP, Items.LIGHT_BLUE_WOOL, Items.LANTERN);
+        offerLampRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, ModBlocks.YELLOW_LAMP, Items.YELLOW_WOOL, Items.LANTERN);
+        offerLampRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, ModBlocks.LIME_LAMP, Items.LIME_WOOL, Items.LANTERN);
+        offerLampRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, ModBlocks.PINK_LAMP, Items.PINK_WOOL, Items.LANTERN);
+        offerLampRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, ModBlocks.GRAY_LAMP, Items.GRAY_WOOL, Items.LANTERN);
+        offerLampRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, ModBlocks.LIGHT_GRAY_LAMP, Items.LIGHT_GRAY_WOOL, Items.LANTERN);
+        offerLampRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, ModBlocks.CYAN_LAMP, Items.CYAN_WOOL, Items.LANTERN);
+        offerLampRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, ModBlocks.PURPLE_LAMP, Items.PURPLE_WOOL, Items.LANTERN);
+        offerLampRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, ModBlocks.BLUE_LAMP, Items.BLUE_WOOL, Items.LANTERN);
+        offerLampRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, ModBlocks.BROWN_LAMP, Items.BROWN_WOOL, Items.LANTERN);
+        offerLampRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, ModBlocks.GREEN_LAMP, Items.GREEN_WOOL, Items.LANTERN);
+        offerLampRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, ModBlocks.RED_LAMP, Items.RED_WOOL, Items.LANTERN);
+        offerLampRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, ModBlocks.BLACK_LAMP, Items.BLACK_WOOL, Items.LANTERN);
     }
 }
