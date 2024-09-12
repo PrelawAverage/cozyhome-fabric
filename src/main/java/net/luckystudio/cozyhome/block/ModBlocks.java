d9e431d39abc4ee0e181287a517d7eef8bc008f8
PrelawAverage <bryanguzman859@gmail.com>
2024-09-12 16:46:16 -0400

    - Pulled all changes from Master into this branch to work with current state of the project

--- src/main/java/net/luckystudio/cozyhome/block/ModBlocks.java
+++ src/main/java/net/luckystudio/cozyhome/block/ModBlocks.java
@@ -1,15 +1,11 @@
 package net.luckystudio.cozyhome.block;
 
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.luckystudio.cozyhome.CozyHome;
 import net.luckystudio.cozyhome.block.custom.LampBlock;
 import net.luckystudio.cozyhome.block.custom.PlankedWallBlock;
-import net.luckystudio.cozyhome.item.ModItemGroups;
 import net.minecraft.block.*;
-import net.minecraft.block.enums.NoteBlockInstrument;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
