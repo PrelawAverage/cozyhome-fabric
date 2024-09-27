481488f14c9bb30feee8b11da86f8b7e8b4dd94a
PrelawAverage <bryanguzman859@gmail.com>
2024-09-26 22:27:42 -0400

    - Added some datagens

--- src/main/java/net/luckystudio/cozyhome/datagen/ModModelProvider.java
+++ src/main/java/net/luckystudio/cozyhome/datagen/ModModelProvider.java
@@ -2,6 +2,7 @@ package net.luckystudio.cozyhome.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
+import net.luckystudio.cozyhome.block.ModBlocks;
 import net.minecraft.data.client.BlockStateModelGenerator;
 import net.minecraft.data.client.ItemModelGenerator;
 
@@ -10,13 +11,13 @@ public class ModModelProvider extends FabricModelProvider {
         super(output);
     }
 
+    // Even though it states "blockstate", these will generate blockstates, models, and item models.
     @Override
     public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
-
+        blockStateModelGenerator.registerGlassPane(ModBlocks.AUTUMN_STAINED_WINDOW, ModBlocks.AUTUMN_STAINED_WINDOW_PANE);
     }
 
     @Override
     public void generateItemModels(ItemModelGenerator itemModelGenerator) {
-
     }
 }
