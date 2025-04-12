dc6174ea6fac11d703acd21b5e087c4b19f42a42
Bryan <bryanguzman859@gmail.com>
2025-04-12 16:34:15 -0400

    - Clean main class

--- src/main/java/net/luckystudio/cozyhome/CozyHome.java
+++ src/main/java/net/luckystudio/cozyhome/CozyHome.java
@@ -1,6 +1,7 @@
 package net.luckystudio.cozyhome;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.object.builder.v1.trade.TradeOfferHelper;
 import net.luckystudio.cozyhome.block.ModBlocks;
 import net.luckystudio.cozyhome.block.util.ModBlockEntityTypes;
 import net.luckystudio.cozyhome.components.ModDataComponents;
@@ -10,7 +11,12 @@ import net.luckystudio.cozyhome.item.ModItems;
 import net.luckystudio.cozyhome.util.ModFlammableBlocks;
 import net.luckystudio.cozyhome.util.ModFuels;
 import net.luckystudio.cozyhome.util.ModSoundEvents;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
+import net.minecraft.village.TradeOffer;
+import net.minecraft.village.TradedItem;
+import net.minecraft.village.VillagerProfession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
